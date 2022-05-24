1a43b1c121a68a4bf205a9f0831e0656ddd08bfb
kdipto1 <kdipto@outlook.com>
2022-05-24 20:12:51 +0600

    Blogs page added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -30,11 +30,42 @@ const Blogs = () => {
       <div>
         <h2 className="text-2xl">How does prototypical inheritance work?</h2>
         <p>
-          <small>Ans:</small> The Prototypal Inheritance is a feature in
-          javascript used to add methods and properties in objects. It is a
-          method by which an object can inherit the properties and methods of
-          another object. Traditionally, in order to get and set the
-          [[Prototype]] of an object, we use Object.getPrototypeOf and Object.
+          <small>Ans:</small> The prototypical inheritance refers to the ability
+          to access object properties from another object. We use a JavaScript
+          prototype to add new properties and methods to an existing object
+          constructor. We can then essentially tell our JS code to inherit
+          properties from a prototype. Prototypical inheritance allows us to
+          reuse the properties or methods from one JavaScript object to another
+          through a reference pointer function.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-2xl">
+          Why you do not set the state directly in React. For example, if you
+          have const [products, setProducts] = useState([]). Why you do not set
+          products = [...] instead, you use the setProducts?
+        </h2>
+        <p>
+          <small>Ans:</small>If we directly set products , instead of using
+          setProducts then it will not update the state immediately . instead it
+          will create a pending state transition , and accessing it after
+          calling this method will only return the present value. So, by doing
+          this we can lose control of the state across all components.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-2xl">What is a unit test?</h2>
+        <p>
+          <small>Ans:</small>Unit testing is a type of software testing where
+          individual units or components of a software are tested.
+        </p>
+        <h2 className="text-2xl">Why should write unit tests?</h2>
+        <p>
+          <small>Ans:</small>The purpose is to validate that each unit of the
+          software code performs as expected. Unit Testing is done during the
+          development (coding phase) of an application by the developers. Unit
+          Tests isolate a section of code and verify its correctness. A unit may
+          be an individual function, method, procedure, module, or object.
         </p>
       </div>
     </div>
